2abf0aa645e029124b330bafd14a656a001a278a
morgancivida@gmail.com <morgancivida@gmail.com>
2020-10-29 02:41:37 -0700

    quiz finished, covid-19 information finished

---
+++ quiz.js
@@ -0,0 +1,47 @@
+var counter = 0;
+
+function checkInput()
+{
+    var cq1 = document.getElementById('q1_b').checked;
+    var cq2 = document.getElementById('q2_d').checked;
+    var cq3 = document.getElementById('q3_a').checked;
+    var cq4 = document.getElementById('q4_d').checked;
+    var cq5 = document.getElementById('q5_c').checked;
+
+    if(cq1 == true)
+    {
+        counter++;
+        document.getElementById('counter').innerHTML = counter;
+    }
+
+    if(cq2 == true)
+    {
+        document.getElementById('counter').innerHTML = counter;
+    }
+
+    if(cq3 == true)
+    {
+        document.getElementById('counter').innerHTML = counter;
+    }
+
+    if(cq4 == true)
+    {
+        document.getElementById('counter').innerHTML = counter;
+    }
+
+    if(cq5 == true)
+    {
+        document.getElementById('counter').innerHTML = counter;
+    }
+}
+
+function calculate()
+{
+    var numOfQuestions = 5;
+    if(counter > numOfQuestions)
+    {
+        counter = numOfQuestions;
+    }
+    var result = counter + " / 5";
+    document.getElementById('result').innerHTML = result;
+}
